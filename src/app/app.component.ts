a7e08a6400f3f5023e2a99415baf1856c5a3cc81
FazioNico <contact@nicolasfazio.ch>
2021-04-14 12:37:35 +0200

    upd: display install notif for iOs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PwaDemo';
+
+  constructor(
+    private _toast: ToastController
+  ) {
+    this.checkDevice();
+  }
+
+  checkDevice() {
+    // Detects if device is on iOS
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test( userAgent );
+    };
+    // Detect if app is install
+    const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+      this.displayPopUpInstall();
+    }
+  }
+
+  async displayPopUpInstall() {
+    const toast = await this._toast.create({
+      message: `Pour installer l'application...`,
+      position: 'bottom',
+      keyboardClose: true,
+      color: 'dark',
+      buttons: [
+        {
+          text: 'ok',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+    await toast.present();
+  }
 }
